906528409fa7bdf1fd5e5bf0aeea1f6cefb8430a
DesCritas <nik1234nik@gmail.com>
2023-10-02 09:39:42 +0300

    ver1 app, it can get response from server and show flight list on first screen + navigation works correctly + like function realized correctly. todo: refactor and optimize api service, new design

--- app/src/main/java/com/descritas/timetotrip/activity/FlightDetail.kt
+++ app/src/main/java/com/descritas/timetotrip/activity/FlightDetail.kt
@@ -5,45 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
-
-import com.descritas.timetotrip.R
+import com.descritas.timetotrip.databinding.FragmentFlightDetailBinding
 import com.descritas.timetotrip.dto.Flight
 import com.descritas.timetotrip.viewModel.FlightViewModel
-
 
 class FlightDetail : Fragment() {
     private val args: FlightDetailArgs by navArgs()
     private lateinit var flight: Flight
     private val viewModel: FlightViewModel by activityViewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        flight = args.flight
+        val binding = FragmentFlightDetailBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        bindLayout(binding)
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        return binding.root
 
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        flight = args.flight
-        val view = inflater.inflate(R.layout.fragment_flight_detail, container, false)
-
-
-        val detailDeparture: TextView = view.findViewById(R.id.detailDeparture)
-        val detailArrival: TextView = view.findViewById(R.id.detailArrival)
-        val detailStartDate: TextView = view.findViewById(R.id.detailStartTime)
-        val detailEndDate: TextView = view.findViewById(R.id.detailEndTime)
-        val detailPrice: TextView = view.findViewById(R.id.detailPrice)
-
-        detailDeparture.text = flight.departure
-        detailArrival.text = flight.arrival
-        detailStartDate.text = flight.startDate
-        detailEndDate.text = flight.endDate
-        detailPrice.text = flight.price
-
-
-        return view
+    private fun bindLayout(binding: FragmentFlightDetailBinding) {
+        with(binding) {
+            detailDeparture.text = flight.departure
+            detailArrival.text = flight.arrival
+            detailStartTime.text = flight.startDate
+            detailEndTime.text = flight.endDate
+            detailPrice.text = flight.price
+            like.isChecked = flight.liked
+            like.setOnClickListener {
+                viewModel.likeById(flight)
+            }
+        }
     }
 
     companion object {
@@ -56,6 +56,4 @@ class FlightDetail : Fragment() {
             return fragment
         }
     }
-
-
 }
